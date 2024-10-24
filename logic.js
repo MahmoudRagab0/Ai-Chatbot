3a5684f70eda869501fe7b376e4094c583a56ce9
Mahmoud Ragab <mahmoud06ragab@gmail.com>
2024-10-24 17:49:02 +0300

    upload-files

--- logic.js
+++ logic.js
@@ -98,10 +98,6 @@ message_input.addEventListener("keydown", (e) => {
     const user_message = e.target.value.trim();
     if (e.key === "Enter" && user_message) {
         handleOutgoingMessage(e);
-        chat_body.scrollTo({
-            top: chat_body.scrollHeight,
-            behavior: "smooth",
-        });
     }
 });
 
